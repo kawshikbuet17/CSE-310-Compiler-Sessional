fe6785200935cc3fc7d649b2331a55d23e2fcac2
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2021-03-14 02:41:59 +0600

    Scopetable, Symboltable print functions added

--- Offline-01-Symbol-Table/symbol_table.cpp
+++ Offline-01-Symbol-Table/symbol_table.cpp
@@ -40,16 +40,20 @@ public:
 
 class ScopeTable
 {
-    int id;
+    string id;
     int totalBuckets;
     vector<SymbolInfo> *hashTable;
     ScopeTable* parentScope;
+    int childCount;
 
 public:
-    ScopeTable(int N)
+    ScopeTable(int N, string id)
     {
         totalBuckets = N;
+        this->id = id;
         hashTable = new vector<SymbolInfo>[totalBuckets];
+        parentScope = NIL;
+        childCount = 0;
     }
 
     ~ScopeTable()
@@ -67,6 +71,21 @@ public:
         return parentScope;
     }
 
+    string getId()
+    {
+        return id;
+    }
+
+    int setChildCount(int n)
+    {
+        childCount = n;
+    }
+
+    int getChildCount()
+    {
+        return childCount;
+    }
+
     void setHashTableSize(int N)
     {
         this->totalBuckets = N;
@@ -74,6 +93,7 @@ public:
 
     int hashFunction(string str)
     {
+        cout<<"ScopeTable hashFunction"<<endl;
         int hashVal = 0;
         int sumAscii = 0;
         for(int i=0; i<str.length(); i++)
@@ -81,27 +101,30 @@ public:
             sumAscii += str[i];
         }
         hashVal = sumAscii % totalBuckets;
+        //cout<<"hashVal "<<hashVal<<endl;
         return hashVal;
     }
 
     bool Insert(SymbolInfo symbol)
     {
+        cout<<"ScopeTable Insert"<<endl;
         SymbolInfo* location = NIL;
-        location = Lookup(symbol);
+        location = Lookup(symbol.getSymbolName());
         if(location == NIL)
         {
             int hashVal = hashFunction(symbol.getSymbolName());
             hashTable[hashVal].push_back(symbol);
+            cout<<"Inserted in ScopeTable# "<< id <<" at position "<< hashVal <<", " << hashTable[hashVal].size()-1 << endl;
             return true;
         }
         else
             return false;
     }
 
-    bool Delete(SymbolInfo symbol)
+    bool Delete(string symbolName)
     {
-        int hashVal = hashFunction(symbol.getSymbolName());
-        SymbolInfo* location = Lookup(symbol);
+        int hashVal = hashFunction(symbolName);
+        SymbolInfo* location = Lookup(symbolName);
         if(location != NIL)
         {
             swap(*location, hashTable[hashVal][hashTable[hashVal].size()-1]);
@@ -112,13 +135,14 @@ public:
             return false;
     }
 
-    SymbolInfo* Lookup(SymbolInfo symbol)
+    SymbolInfo* Lookup(string symbolName)
     {
-        int hashVal = hashFunction(symbol.getSymbolName());
+        cout<<"ScopeTable Lookup"<<endl;
+        int hashVal = hashFunction(symbolName);
         SymbolInfo* temp = NIL;
         for(int i=0; i<hashTable[hashVal].size(); i++)
         {
-            if(hashTable[hashVal][i].getSymbolName() == symbol.getSymbolName() && hashTable[hashVal][i].getSymbolType() == symbol.getSymbolType())
+            if(hashTable[hashVal][i].getSymbolName() == symbolName)
             {
                 temp = &hashTable[hashVal][i];
                 break;
@@ -127,7 +151,19 @@ public:
         return temp;
     }
 
-    Print();
+    void Print()
+    {
+        cout << "ScopeTable # " << id << endl;
+        for(int i=0; i<totalBuckets; i++)
+        {
+            cout << i << " --> ";
+            for(auto j : hashTable[i])
+            {
+                cout<< "<" << j.getSymbolName() << " : " << j.getSymbolType() << "> ";
+            }
+            cout<<endl;
+        }
+    }
 };
 
 
@@ -137,38 +173,60 @@ class SymbolTable
     ScopeTable* previous;
     ScopeTable* current;
     ScopeTable* temp;
+
     int totalBucket;
+    string previousCount;
+    int currentCount;
 
 public:
-    EnterScope()
+    SymbolTable(int N)
     {
-        temp = new ScopeTable(totalBucket);
-        current = temp;
-        current->makeParentScope(previous);
+        totalBucket = N;
+        previousCount = "1";
+        current = new ScopeTable(totalBucket, previousCount);
+        current->makeParentScope(NIL);
+        previous = current;
+        temp = NIL;
     }
 
-    ExitScope()
+    ScopeTable* getCurrentScope()
     {
-        previous = current;
-        current = NIL;
+        return current;
+    }
+    void EnterScope()
+    {
+        cout<<"EnterScope currentCount "<<current<<endl;
+        string newId = current->getId() + "." + to_string(current->getChildCount()+1);
+        current->setChildCount(current->getChildCount()+1);
+        temp = new ScopeTable(totalBucket, newId);
+        temp->makeParentScope(current);
+        current = temp;
+        cout << "New ScopeTable with id " << current->getId() << " created" << endl;
+    }
+
+    void ExitScope()
+    {
+        current = current->getParentScope();
     }
 
     bool Insert(SymbolInfo symbol)
     {
+        cout<<"SymbolTable Insert"<<endl;
         return current->Insert(symbol);
     }
 
-    bool Remove(SymbolInfo symbol)
+    bool Remove(string symbolName)
     {
-        return current->Delete(symbol);
+        return current->Delete(symbolName);
     }
 
-    SymbolInfo* Lookup(SymbolInfo symbol)
+    SymbolInfo* Lookup(string symbolName)
     {
+        cout<<"SymbolTable Lookup"<<endl;
         temp = current;
         while(temp != NIL)
         {
-            if(temp->Lookup(symbol) != NIL)
+            if(temp->Lookup(symbolName) != NIL)
             {
                 break;
             }
@@ -177,19 +235,78 @@ public:
                 temp = temp->getParentScope();
             }
         }
-        return temp->Lookup(symbol);
+        return temp->Lookup(symbolName);
     }
-    PrintAllTables();
-    PrintCurrentTable();
+    void PrintAllTables()
+    {
+        temp = current;
+        while(temp != NIL)
+        {
+            PrintCurrentTable(temp);
+            temp = temp->getParentScope();
+        }
+    }
+    void PrintCurrentTable(ScopeTable* st)
+    {
+        st->Print();
+    }
 };
 
 
 int32_t main()
 {
     int N;
-    cin>>N;
-    while(1)
-    {
+    cin >> N;
 
+    SymbolTable* st = new SymbolTable(N);
+    char c;
+
+    while(cin>>c)
+    {
+        string symbolName, symbolType;
+
+        if(c == 'I')
+        {
+            cin>>symbolName>>symbolType;
+            SymbolInfo* si = new SymbolInfo(symbolName, symbolType);
+            st->Insert(*si);
+        }
+
+        else if(c == 'L')
+        {
+            cin>>symbolName;
+            st->Lookup(symbolName);
+        }
+
+        else if(c == 'D')
+        {
+            cin>>symbolName;
+            st->Remove(symbolName);
+        }
+
+        else if(c == 'P')
+        {
+            char p;
+            cin >> p;
+            if(p == 'A')
+            {
+                st->PrintAllTables();
+            }
+
+            else if(p == 'C')
+            {
+                st->PrintCurrentTable(st->getCurrentScope());
+            }
+        }
+
+        else if(c == 'S')
+        {
+            st->EnterScope();
+        }
+
+        else if(c == 'E')
+        {
+            st->ExitScope();
+        }
     }
 }
